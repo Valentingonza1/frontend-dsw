22da5b68aa3fed89b67fc3e8e5a4cfe4a3abc12a
felascarp <felipechapano@gmail.com>
2025-10-02 11:07:26 -0300

    Elimino carpeta duplicada

--- frontend-dsw-main/src/components/header.jsx
+++
@@ -1,9 +0,0 @@
-function Header() {
-  return (
-    <header style={{ padding: '1rem', backgroundColor: '#900C3F', color: 'white' }}>
-      <h1 style={{ margin: 0 }}>Carnicería El Buen Corte</h1>
-    </header>
-  );
-}
-
-export default Header;
